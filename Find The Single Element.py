c816e5bb74d20f46d2b9ce81926878eabfbf8169
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-02-18 22:53:44 +0530

    Update Find The Single Element.py

--- Find The Single Element.py
+++ Find The Single Element.py
@@ -5,7 +5,10 @@ from typing import *
 def getSingleElement(arr : List[int]) -> int:
     # Write your code here.
     i=1
+    if len(arr)>1 and arr[0]!=arr[1]:
+        return arr[0]
     while i<len(arr)-2:
+
         if arr[i]==arr[i+1]:
             i+=2
         elif arr[i]==arr[i-1]:
@@ -13,3 +16,4 @@ def getSingleElement(arr : List[int]) -> int:
         else:
             return arr[i]
     return arr[len(arr)-1]
+        
