ea65c8be1ae3dcaff366b419fe968250f4530f52
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-03-23 00:46:40 +0530

    Create Longest Increasing Subsequence.py

---
+++ Longest Increasing Subsequence.py
@@ -0,0 +1,32 @@
+#https://leetcode.com/problems/longest-increasing-subsequence/description/
+
+class Solution:
+    def lengthOfLIS(self, arr: List[int]) -> int:
+        n=len(arr)
+        if n==1:
+            return 1
+        memo=[1 for _ in range(n)]
+        i=1
+        maxLength=1
+        # [2,3,1,5,4]
+        #  j i
+        # [1,2,1,1,1] memo
+
+        # [2,3,1,5,4]
+        #    j   i
+        # [1,2,1,3,1] memo
+        while i<n:
+            j=i-1
+            while j>-1:
+                if memo[i]>j+1:
+                    break
+                if arr[j]<arr[i]:
+                    new_len=memo[j]+1
+                    if new_len>memo[i]:
+                        memo[i]=new_len
+                        #finding maximum of the arrar
+                        if maxLength<new_len:
+                            maxLength=new_len
+                j-=1
+            i+=1
+        return maxLength
