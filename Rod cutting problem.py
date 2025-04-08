32d03e27cac24c64ef2b7287f309e86d409b8144
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-04-08 13:29:02 +0530

    Create Rod cutting problem.py

---
+++ Rod cutting problem.py
@@ -0,0 +1,32 @@
+#https://www.naukri.com/code360/problems/rod-cutting-problem_800284
+
+from sys import stdin
+import sys
+
+def cutRod(price, n):
+
+    # Write your code here.
+    dp=[[0 for _ in range(n+1)] for _ in range(len(price)+1)]
+    for i in range(1,n+1):
+        for j in range(1,len(price)+1):
+            if i>j:
+                dp[i][j]=dp[i-1][j]
+            else:
+                dp[i][j]=max(dp[i-1][j],price[i-1]+max(dp[i-1][j-i],dp[i][j-i]))
+    return dp[n][len(price)]
+
+# Taking input using fast I/O.
+def takeInput():
+    n = int(input())
+
+    price = list(map(int, input().strip().split(" ")))
+
+    return price, n
+
+
+# Main.
+t = int(input())
+while t:
+    price, n = takeInput()
+    print(cutRod(price, n))
+    t = t-1
