35c3e7da8864671dbd96b40fea49b2bf0902c6cf
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-05-06 10:48:50 +0530

    Create Largest of 3 numbers.py

---
+++ Largest of 3 numbers.py
@@ -0,0 +1,10 @@
+#https://www.naukri.com/code360/problems/largest-of-3-numbers_1115772
+
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+#Your code goes here
+lis=list(map(int,input().split()))
+print(max(lis))
