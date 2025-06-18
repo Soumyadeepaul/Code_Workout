ac7b1113970ea9ce5244f2b03bab5537b34eb0c8
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-06-18 22:33:01 +0530

    Update Combination Sum.py

--- Combination Sum.py
+++ Combination Sum.py
@@ -31,6 +31,32 @@ def combSum(ARR, B):
 	result=combination(ARR,[],B,[],0)
 
 	return result
-		
+
+
+
+######################################################################
+#https://leetcode.com/problems/combination-sum/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result=[]
+        
+        def com(candidates,i,stack,target,result):
+            if i>=len(candidates):
+                return [result,stack]
+            summ=sum(stack)
+            if summ==target:
+                result.append(list(stack))
+                return [result,stack]
+            elif summ>target:
+                return [result,stack]
+            stack.append(candidates[i])
+            result,stack=com(candidates,i,stack,target,result)
+            stack.pop()
+            result,stack=com(candidates,i+1,stack,target,result)
+            return [result,stack]
+
+        result,stack=com(candidates,0,[],target,result)
+        return result
 
 
