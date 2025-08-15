d2306aee62da48a0fe2e2450ab6fca7a577c98f1
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-08-16 00:44:00 +0530

    Create Find the Difference.cpp

---
+++ Find the Difference.cpp
@@ -0,0 +1,28 @@
+#https://leetcode.com/problems/find-the-difference/?envType=problem-list-v2&envId=hash-table
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<int,int> hashmap={};
+        for(int i=0;i<s.length();i++){
+            if(hashmap.find(s[i])!=hashmap.end()){
+                hashmap[s[i]]=hashmap[s[i]]+1;
+            }
+            else{
+                hashmap.insert({s[i],1});
+            }
+        }
+        for(int i=0;i<t.length();i++){
+            if(hashmap.find(t[i])!=hashmap.end()){
+                if(hashmap[t[i]]==0){
+                    return t[i];
+                }
+                hashmap[t[i]]=hashmap[t[i]]-1;
+            }
+            else{
+                return t[i];
+            }
+        }
+        return ' ';
+    }
+};
