f87d9cf84d67b604eeda34f1f829133f3fc57dd9
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-08-27 08:30:39 +0530

    Update Subsets II.cpp

--- Subsets II.cpp
+++ Subsets II.cpp
@@ -24,3 +24,28 @@ public:
         return result;
     }
 };
+
+////////////////////////////
+class Solution {
+public:
+    void subset(vector<int>&nums,vector<vector<int>> &result, int index, vector<int> bucket,int l){
+        result.push_back(bucket);
+        for(int i=index;i<l;i++){
+            if(i!=index && nums[i]==nums[i-1]){ //not that same index and also same as previous element -> duplicate
+                continue;
+            }
+            bucket.push_back(nums[i]);
+            subset(nums,result,i+1,bucket,l);
+            bucket.pop_back();
+        }
+        return;
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        vector<int> bucket;
+        int l=nums.size();
+        subset(nums,result,0,bucket,l);
+        return result;
+    }
+};
