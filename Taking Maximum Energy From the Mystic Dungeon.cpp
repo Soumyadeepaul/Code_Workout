94bbfcc7572e7525d87bd3479637239abe76727d
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-10-10 15:47:09 +0530

    Create Taking Maximum Energy From the Mystic Dungeon.cpp

---
+++ Taking Maximum Energy From the Mystic Dungeon.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/taking-maximum-energy-from-the-mystic-dungeon/description/?envType=daily-question&envId=2025-10-10
+
+
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int maxi=INT_MIN;
+        int l=energy.size();
+        int summ;
+        bool take;
+        for(int i=0;i<l;i++){
+            summ=energy[i];
+            take=true;
+            for(int j=i+k;j<l;j+=k){
+                summ+=energy[j];
+                if(summ<0){
+                    if(j+k>=l) take=true;
+                    else take=false;
+                    break;
+                }
+            }
+            if(take==true)
+                maxi=max(summ,maxi);
+        }
+        return maxi;
+    }
+};
