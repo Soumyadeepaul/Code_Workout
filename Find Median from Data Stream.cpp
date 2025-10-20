7a6f7435d5e2a011b050ab2ba15cbcc787c48ac9
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-10-20 11:27:29 +0530

    Update Find Median from Data Stream.cpp

--- Find Median from Data Stream.cpp
+++ Find Median from Data Stream.cpp
@@ -34,3 +34,49 @@ public:
  * obj->addNum(num);
  * double param_2 = obj->findMedian();
  */
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+class MedianFinder {
+private:
+    priority_queue<int> max_heap;//for smaller element
+    priority_queue<int,vector<int>,greater<int>> min_heap;//for greater element
+    int max_heapSize=0;
+    int min_heapSize=0;
+public:
+    MedianFinder() {
+    }
+    
+    void addNum(int num) {
+        max_heap.push(num);
+        min_heap.push(max_heap.top());
+        max_heap.pop();
+        min_heapSize++;
+        if(min_heapSize>max_heapSize){
+            max_heap.push(min_heap.top());
+            min_heap.pop();
+            max_heapSize++;
+            min_heapSize--;
+        }
+    }
+    
+    double findMedian() {
+        double d;
+        if(min_heapSize<max_heapSize){
+            d=max_heap.top();
+        }
+        else{
+            d=(double)(min_heap.top()+max_heap.top())/2.0;
+        }
+        return d;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
