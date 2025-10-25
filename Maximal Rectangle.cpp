2b10f361fe6804c46d060808b2c306c195dc3546
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-10-25 13:23:51 +0530

    Update Maximal Rectangle.cpp

--- Maximal Rectangle.cpp
+++ Maximal Rectangle.cpp
@@ -44,3 +44,64 @@ public:
         return maxi;
     }
 };
+
+
+
+
+//////////////////////////////////////////////////////////////////////
+
+
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int l=matrix[0].size();
+        vector<int> rectangle(l);
+        vector<int> prefix(l,0);
+        vector<int> suffix(l,l);
+        int size=0;
+        for(vector<char> row:matrix){
+            stack<pair<int,int>> suffStack;
+            stack<pair<int,int>> prefStack;
+
+            for(int i=0;i<row.size();i++){
+                if(row[i]=='0') rectangle[i]=0;
+                else rectangle[i]++;
+            }
+            for(int i=0;i<l;i++){
+                while(!prefStack.empty()){
+                    auto t=prefStack.top();
+                    if(t.first>=rectangle[i]){
+                        prefStack.pop();  
+                    }
+                    else{
+                        prefix[i]=t.second+1;
+                        break;
+                    }
+                        
+                }
+                prefStack.push({rectangle[i],i});
+
+
+
+                while(!suffStack.empty()){
+                    auto t=suffStack.top();
+                    if(t.first>=rectangle[l-i-1]){
+                        suffStack.pop();  
+                    }
+                    else{
+                        suffix[l-i-1]=t.second-1; 
+                        break;
+                    }
+                       
+                }
+                suffStack.push({rectangle[l-i-1],l-i});
+            }
+            for(int i=0;i<l;i++){
+                size=max(size,((suffix[i]-prefix[i])*rectangle[i]));
+            }
+            fill(prefix.begin(), prefix.end(), 0);
+            fill(suffix.begin(), suffix.end(), l);
+        }
+        return size;
+    }
+};
