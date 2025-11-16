8ac632fc1445ccd4f8152dbbc4609e023ed0ee1d
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-11-16 21:33:07 +0530

    Update Matrix Chain Multiplication.cpp

--- Matrix Chain Multiplication.cpp
+++ Matrix Chain Multiplication.cpp
@@ -29,3 +29,30 @@ public:
         return dp[0][l - 1];
     }
 };
+
+
+///////////////////////////////////////////////////////////////////////////
+
+
+
+class Solution {
+public:
+    int matrixMultiplication(vector<int> &arr) {
+        int l = arr.size()-1;
+        vector<vector<int>> dp(l, vector<int>(l, 0));
+        for(int gap=0;gap<l;gap++){
+            for(int i=0;i+gap<l;i++){
+                int j=i+gap;
+                if(i==j) dp[i][j]=0;
+                else{
+                    dp[i][j]=INT_MAX;
+                    for(int k=i;k<j;k++){
+                        dp[i][j]=min(dp[i][j],dp[i][k]+dp[k+1][j]+(arr[i]*arr[k+1]*arr[j+1]));
+                    }
+                }
+            }
+        }
+        return dp[0][l - 1];
+    }
+};
+
