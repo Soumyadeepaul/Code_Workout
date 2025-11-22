7690f068753c18c8516580644982ccb544a05437
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-11-22 20:14:14 +0530

    Create Total Waviness of Numbers in Range I.cpp

---
+++ Total Waviness of Numbers in Range I.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/contest/biweekly-contest-170/problems/total-waviness-of-numbers-in-range-i/description/
+
+
+class Solution {
+public:
+    int totalWaviness(int num1, int num2) {
+        int count=0;
+        for(int i=num1;i<=num2;i++){
+            string s=to_string(i);
+            for(int j=1;j<s.size()-1;j++){
+                if((s[j]>s[j-1] && s[j]>s[j+1] )||(s[j]<s[j-1] && s[j]<s[j+1] ))
+                    count++;
+            }
+        }
+        return count;
+    }
+}
