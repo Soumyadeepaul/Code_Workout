6ab7ca229b8bf4291295cd8e84997cd288e49811
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2026-01-29 15:56:51 +0530

    Create Bellman-Ford.cpp

---
+++ Bellman-Ford.cpp
@@ -0,0 +1,32 @@
+//https://www.geeksforgeeks.org/problems/distance-from-the-source-bellman-ford-algorithm/1
+
+
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> bellmanFord(int V, vector<vector<int>>& edges, int src) {
+        // Code here
+        vector<int> dist(V,1e8);
+        dist[src]=0;
+        
+        for(int i=0;i<V-1;i++){
+            for(auto &e:edges){
+                int u=e[0],v=e[1],wt=e[2];
+                
+                if(dist[u]!=1e8 && dist[u]+wt<dist[v]){
+                    dist[v]=dist[u]+wt;
+                }
+            }
+        }
+        
+        //Nth to detect negetive cycle
+        for(int i=0;i<1;i++){
+            for(auto &e:edges){
+                int u=e[0],v=e[1],wt=e[2];
+                if(dist[u]!=1e8 && dist[u]+wt<dist[v]) return {-1};
+            }
+        }
+        return dist;
+    }
+};
