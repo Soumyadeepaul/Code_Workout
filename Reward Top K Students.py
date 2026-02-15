e8b80a8bb662b09aa0802ba30fead0470c46d4f7
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2026-02-15 15:50:31 +0530

    Update and rename Reward Top K Students.cpp to Reward Top K Students.py

--- Reward Top K Students.py
+++ Reward Top K Students.py
@@ -1,4 +1,4 @@
-//https://leetcode.com/problems/reward-top-k-students/description/
+##https://leetcode.com/problems/reward-top-k-students/description/
 
 class Solution:
     def topStudents(self, positive_feedback: List[str], negative_feedback: List[str], report: List[str], student_id: List[int], k: int) -> List[int]:
