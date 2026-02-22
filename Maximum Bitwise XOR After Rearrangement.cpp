333c703f9fc98382f512923412369b3335c8ba2d
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2026-02-22 08:31:08 +0530

    Create Maximum Bitwise XOR After Rearrangement.cpp

---
+++ Maximum Bitwise XOR After Rearrangement.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/contest/weekly-contest-490/problems/maximum-bitwise-xor-after-rearrangement/description/
+
+
+class Solution {
+public:
+    string maximumXor(string s, string t) {
+        int count1=0;
+        for(int i:t){
+            if(i=='1') count1++;
+        }
+        string newT="";
+        for(int i:s){
+            if(i=='0' && count1!=0){
+                newT+='1';
+                count1--;
+            }
+            else newT+='0';
+        }
+        int idx=s.size()-1;
+        while(count1!=0){
+            if(newT[idx]=='0'){
+                newT[idx]='1';
+                count1--;
+            }
+            idx--;
+        }
+        string result="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=newT[i]) result+='1';
+            else result+='0';
+        }
+        return result;
+    }
+};©leetcode
