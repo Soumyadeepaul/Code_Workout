4fd787b3c752eb6e4a3facae0dc655b4a7184a83
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2026-04-17 18:49:22 +0530

    Implement minMirrorPairDistance function

--- Minimum Absolute Distance Between Mirror Pairs.cpp
+++ Minimum Absolute Distance Between Mirror Pairs.cpp
@@ -27,3 +27,41 @@ public:
         return result==INT_MAX ? -1 : result;
     }
 };
+
+
+/////////////////////////////////////////////////////////
+//SOLVED
+
+
+
+class Solution {
+public:
+    int helper(int n){
+        
+        string s=to_string(n);
+        int l=s.size()-1;
+        while(s[l]=='0'){
+            s.erase(l);
+            l--;
+        }
+        reverse(s.begin(),s.end());
+        int newN=stoi(s);
+        return newN;
+    }
+    int minMirrorPairDistance(vector<int>& nums) {
+        unordered_map<int,vector<int>> map;
+        for(int i=0;i<nums.size();i++){
+            map[nums[i]].emplace_back(i);
+        }
+        int result=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            int rev=helper(nums[i]);
+            vector<int> &idx=map[rev];
+            if(idx.size()==0) continue;
+            auto add=upper_bound(idx.begin(),idx.end(),i);
+            if(add==idx.end()) continue;
+            result=min(result,abs(i-*add));
+        }
+        return (result==INT_MAX)? -1:result;
+    }
+};
