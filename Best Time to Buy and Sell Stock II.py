7403c57217ad645ab4933166cb473072979f6f9e
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2026-06-04 09:39:40 +0530

    Refactor maxProfit to use dynamic programming
    Refactored the maxProfit function to use dynamic programming and added a helper function for better readability and efficiency.

--- Best Time to Buy and Sell Stock II.py
+++ Best Time to Buy and Sell Stock II.py
@@ -22,3 +22,53 @@ class Solution:
                     profit+=prices[i]-minima
                     minima=10**9+7
         return profit
+///////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        bool sell=false;
+        int profit=0;
+        int buy=INT_MAX;
+        int sell_day=0;
+        prices.push_back(INT_MIN);
+        for(int i:prices){
+            if(sell==false){
+                if(buy>i) buy=i;
+                else sell=true;
+            }
+            if(sell==true){
+                if(sell_day<i) sell_day=i;
+                else {
+                    profit+=sell_day-buy;
+                    sell_day=0;
+                    sell=false;
+                    buy=i;
+                }
+            }
+        }
+        return profit;
+    }
+};
+
+///////////////////////////////////////////////////////
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int helper(vector<int>& prices, int idx, bool buy){
+        if(idx==prices.size()) return 0;
+        if(dp[idx][buy]!=-1) return dp[idx][buy];
+        if(buy){
+            return dp[idx][buy]=max(helper(prices,idx+1,false)-prices[idx],helper(prices,idx+1,true));
+        }
+        else{
+            return dp[idx][buy]=max(helper(prices,idx+1,true)+prices[idx],helper(prices,idx+1,false));
+        }
+
+    }
+    int maxProfit(vector<int>& prices) {
+        dp.resize(prices.size(),vector<int> (2,-1));
+        return helper(prices,0,true);
+    }
+};
